79cfb1b2bc09fda96acefb7a3ed50edd876d535d
michaelmaloof1991@gmail.com <michaelmaloof1991@gmail.com>
2014-10-05 01:00:14 -0500

    results done

--- WikipediaRaces/ResultsViewController.m
+++ WikipediaRaces/ResultsViewController.m
@@ -14,6 +14,10 @@
 @property (strong, nonatomic) IBOutlet UILabel *totalGameTime;
 @property NSString *finalScore;
 @property (weak, nonatomic) IBOutlet UILabel *clickScoreTotalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property NSInteger totalScoreForGame;
+@property (weak, nonatomic) IBOutlet UILabel *timerScoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalScoreLabel;
 
 
 @end
@@ -22,36 +26,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    uint32_t rnd = arc4random_uniform(100);
-    NSInteger finalScoreInt = rnd;
-    self.finalScore = [NSString stringWithFormat:@"%ld", (long)finalScoreInt];
+
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    self.numberOfGameClicks.text = self.clickCounterData;
+    self.timerLabel.text = self.timerData;;
+    NSInteger timerInt = [self.timerData intValue];
+    NSInteger timerScore = 500 - timerInt;
+    NSInteger clicks = [self.numberOfGameClicks.text integerValue];
+    NSInteger clickScore = 500 - (clicks * 20);
+
+    self.totalScoreForGame = timerScore + clickScore;
+    self.finalScore = [NSString stringWithFormat:@"%ld", (long)self.totalScoreForGame];
 
 
     NSUserDefaults *saved = [NSUserDefaults standardUserDefaults];
     NSString *stringHighScoreCurrent = [saved stringForKey:@"UserHighScore"];
     NSInteger highScoreInt = [stringHighScoreCurrent integerValue];
 
-    if (finalScoreInt > highScoreInt) {
+    if (self.totalScoreForGame > highScoreInt) {
 
-    NSUserDefaults *saveHighScore = [NSUserDefaults standardUserDefaults];
-    [saveHighScore setObject: self.finalScore forKey:@"UserHighScore"];
+        NSUserDefaults *saveHighScore = [NSUserDefaults standardUserDefaults];
+        [saveHighScore setObject: self.finalScore forKey:@"UserHighScore"];
 
     }
-    
+
     NSUserDefaults *totalScore = [NSUserDefaults standardUserDefaults];
     NSInteger currentTotalScore = [totalScore integerForKey:@"Current Total"];
-    currentTotalScore += finalScoreInt;
+    currentTotalScore += self.totalScoreForGame;
     [totalScore setInteger: currentTotalScore forKey:@"Current Total"];
 
-
-
-}
-
--(void)viewDidAppear:(BOOL)animated{
-    self.numberOfGameClicks.text = self.clickCounterData;
-    NSInteger clicks = [self.numberOfGameClicks.text integerValue];
-    NSInteger clickScore = 500 - (clicks * 20);
+    self.timerScoreLabel.text =[NSString stringWithFormat:@"%ld", (long)timerScore];
     self.clickScoreTotalLabel.text = [NSString stringWithFormat:@"%ld", (long)clickScore];
+    self.totalScoreLabel.text = [NSString stringWithFormat:@"%ld", (long)self.totalScoreForGame];
     
 
 }
